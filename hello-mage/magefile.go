3a16dfeb6b6e42249b3bb1b139afbf03e5b164dc
mkeretic1 <keretic.mateo@gmail.com>
2022-11-20 18:06:54 +0100

    Change magefile method name

--- hello-mage/magefile.go
+++ hello-mage/magefile.go
@@ -7,7 +7,7 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-func Exec(os string, arch string, projectBuildDir string, projectName string) error {
+func Build(os string, arch string, projectBuildDir string, projectName string) error {
 	goos := fmt.Sprintf("GOOS=%s", os)
 	goarch := fmt.Sprintf("GOARCH=%s", arch)
 	outputLocation := fmt.Sprintf("%s/%s-%s/%s", projectBuildDir, os, arch, projectName)
